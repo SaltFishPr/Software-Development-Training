60c848ecdda062e6147a858f155b531a7d03f854
QiQi-OvO <2763359332@qq.com>
2020-01-06 03:19:16 +0800

    commit_test

--- journal_management/settings.py
+++ journal_management/settings.py
@@ -78,10 +78,6 @@ WSGI_APPLICATION = 'journal_management.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-    }
 }
 
 
--- journal_management_system/sqlite_service_test.py
+++ journal_management_system/sqlite_service_test.py
@@ -1,7 +1,16 @@
 from database import user
 from database import record
-print(record.ask_record_by_account('jl'))
-
-# dict1 =
-# for i in record.ask_record_by_account('jl'):
-#     x = list
+from database import journal
+#print(record.ask_record_by_account('jl'))
+# key_list=record.ask_key_by_account('jl')
+# info_len=record.get_info_account('jl')
+# borrow_list=[]
+# borrow={
+#     'name':"",
+#     'status':"",
+#     'time_1':"",
+#     'time_2':""
+# }
+# for i in range(info_len):
+#     borrow['name']=journal.get_name_by_key(key_list[i])
+#     borrow['status']=
