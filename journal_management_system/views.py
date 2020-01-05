6030459c396788d45bc2a4a0ef9c5b241fec6da8
saltfish <saltfishpr@gmail.com>
2020-01-05 20:11:41 +0800

    Update views.py

--- journal_management_system/views.py
+++ journal_management_system/views.py
@@ -9,41 +9,47 @@ def fun():
 
 
 def register_page(request):
-    return render(request,'user_register_page.html')
+    return render(request, 'user_register_page.html')
 
 
 def register_judge(request):
     account = request.GET.get('username')
-    password=request.GET.get('password')
-    #name=request.GET.get('name')
-    name='OvO'
-    identity='reader'
-    grade=1
-    dict1={
+    password = request.GET.get('password')
+    # name=request.GET.get('name')
+    name = 'OvO'
+    identity = 'reader'
+    grade = 1
+    dict1 = {
 
     }
-    data={
-        'dict':dict1
+    data = {
+        'dict': dict1
     }
-    #执行数据库的插入操作
-    if(user.check_account_exsit(account)==True):#如果存在相同的账户
-        dict1['flag']=0
+    # 执行数据库的插入操作
+    if (user.check_account_exsit(account) == True):  # 如果存在相同的账户
+        dict1['flag'] = 0
     else:
         user.add_user(account, password, name, identity, grade)
-        dict1['flag']=1
+        dict1['flag'] = 1
     return JsonResponse(data)
+
+
 def register_return(request):
-    return render(request,'login_page.html')
+    return render(request, 'login_page.html')
+
+
 def login_page(request):
-    return render(request,'login_page.html')
+    return render(request, 'login_page.html')
+
+
 def login_judge(request):
-    dict1={
-        'flag':-1
+    dict1 = {
+        'flag': -1
     }
-    data={
-        'dict':dict1,
-        'pageTitle':8798,
-        'pageData':66
+    data = {
+        'dict': dict1,
+        'pageTitle': 8798,
+        'pageData': 66
     }
     print("没有获取数据")
     Username = str(request.GET.get('username'))
@@ -51,10 +57,10 @@ def login_judge(request):
     print('获取到了数据')
     print(Username)
     print(Password)
-    if(user.check_account_exsit(Username)==False):
+    if (user.check_account_exsit(Username) == False):
         dict1['flag'] = -1
     else:
-        if(user.check_password_exsit(Username)==str(Password)):
+        if (user.check_password_exsit(Username) == str(Password)):
             dict1['flag'] = 1
             if user.ask_user_identity(Username) == 'admin':
                 dict1['flag'] = 2
@@ -64,17 +70,31 @@ def login_judge(request):
             dict1['flag'] = 0
     print(dict1['flag'])
     return JsonResponse(data)
+
+
 def login_return(request):
-    return render(request,'user_center_page.html')
+    return render(request, 'user_center_page.html')
+
+
 def user_center_page(request):
-    return render(request,'user_center_page.html')
+    return render(request, 'user_center_page.html')
+
+
 def system_admin_center_page(request):
-    return render(request,'system_admin_center_page.html')
+    return render(request, 'system_admin_center_page.html')
+
+
 def journal_admin_center_page(request):
-    return render(request,'journal_admin_center_page.html')
+    return render(request, 'journal_admin_center_page.html')
+
+
 def user_data_page(request):
-    return render(request,'user_data_page.html')
+    return render(request, 'user_data_page.html')
+
+
 def user_borrow_page(request):
-    return render(request,'user_borrow_page.html')
+    return render(request, 'user_borrow_page.html')
+
+
 def journal_admin_store_page(request):
-    return render(request,'journal_admin_store_page.html')
+    return render(request, 'journal_admin_store_page.html')
